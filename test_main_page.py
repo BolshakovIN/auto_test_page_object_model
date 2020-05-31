bbf052370c7b206d28cdd2dd4548119172d0033c
ivanbolsakov <Volvofh12380>
2020-05-31 19:24:09 +0300

    new case with try/except

--- test_main_page.py
+++ test_main_page.py
@@ -4,11 +4,21 @@ from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page = MainPage(browser,link)
+    page = MainPage(browser, link)
     # открываем страницу
     page.open()
     # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
+    #page.should_be_login_link()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
 
     #browser.get(link)
     #login_link = browser.find_element_by_css_selector("#login_link")
